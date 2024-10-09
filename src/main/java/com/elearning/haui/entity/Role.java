46ee1d99959028e2227ae616229da81a25d84497
huy2004-newbie <hahuy1810@gmail.com>
2024-10-10 03:02:47 +0700

    ✨ Init Role

---
+++ src/main/java/com/elearning/haui/entity/Role.java
@@ -0,0 +1,32 @@
+package com.elearning.haui.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
+
+}
