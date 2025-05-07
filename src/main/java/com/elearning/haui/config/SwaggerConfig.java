041e7aab41ed2e4cb2dd99ae0f6cd152184b108d
DinhLuong04 <166143726+DinhLuong04@users.noreply.github.com>
2025-05-07 20:56:25 +0700

    Add JWT authentication to Swagger config 7/5/2025

--- src/main/java/com/elearning/haui/config/SwaggerConfig.java
+++ src/main/java/com/elearning/haui/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package com.elearning.haui.config;
 
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,13 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Demo API")
                         .version("1.0")
-                        .description("API documentation with Swagger"));
+                        .description("API documentation with Swagger"))
+                         .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("JWT", new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
